17e20e6f188dfffeccbe47ee3043f497bfa22a1a
Maysha-Khanom-Moon <maysha412@gmail.com>
2026-07-14 20:33:06 +0600

    basic python input, types practices

---
+++ Module 1/practice.py
@@ -0,0 +1,8 @@
+# print("Your age is " + 24)
+print("Your age is " + str(24))
+
+print("Py" + "thon")
+print("Hi" * 3)
+
+print(4/0)
+print(4/2)
---
+++ Module 1/task.py
@@ -0,0 +1,11 @@
+# str, int, float, bool
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+print("Sum:", int(a) + int(b))
+print("Product:", int(a) * int(b))
+print("Remainder:", int(a) % int(b), '\n')
+
+print("Result:", a + b, '\n')
+
+print("Float", float(a))
